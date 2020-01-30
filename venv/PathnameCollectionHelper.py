cc8efeed6d1a73bfae35df791bf7c0b848e174b6
lquante <lennart.quante@me.com>
2020-01-30 18:33:15 +0100

    initial commit
    - identification and grouping of filepaths
    - first implementation of generation of multiple settings files sorted by directories and timeperiod covered

--- venv/PathnameCollectionHelper.py
+++ venv/PathnameCollectionHelper.py
@@ -4,6 +4,7 @@
 import argparse
 import os
 import glob
+from array import array
 from typing import Optional, Match
 
 import yaml
@@ -58,7 +59,9 @@ args = parser.parse_args()
 
 # default root directory
 if not args.root:
-    args.root = os.getcwd()
+    args.root = os.path.dirname("/home/maxkotz/Desktop/QuanteLennart_projects")
+
+        #os.getcwd()
 
 if not os.path.exists(args.root):
     exit("Model binary '{}' not found".format(args.model))
@@ -75,7 +78,7 @@ if not args.blueprint:
 if not args.fileextensions:
     args.fileextensions =  ("nc")
 
-# default - empty- search term to look for
+# default -  search terms to look for TODO: generalize search term default via blueprint.yml
 if not args.searchterms:
     args.searchterms =  (["pr","prsn","tas"])
 
@@ -103,29 +106,55 @@ with open(args.blueprint, 'r') as stream:
         settings = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
-searchresults = {}
+# define data sets to store results
+directories = set()
+timeperiods = set()
 
-#sort file paths
-for i_searchterm in range(len(args.searchterms)):
-    searchterm = args.searchterms[i_searchterm]
+searchresults = {} #TODO implement completness check for results, i.e. relax assumption that always all search variables can be found
+#find relevant directories for each search term
+for i_searchterm in args.searchterms:
     results_for_searchterm=[]
-    for i_filenames in range (len(filenames[searchterm])):
-        searchresult = re.search(('.*'+searchterm+'(\_+)'), filenames[searchterm][i_filenames])
-        if searchresult:
-            results_for_searchterm.append(searchresult.string)
-    searchresults[searchterm]=(results_for_searchterm)
-# TODO implement auto grouping of paths
+    searchpattern = ('(?<=/).*' + i_searchterm + '(\_+)+.*\d{8}-\d{8}\.nc')
+    # get directory path to group files accordingly, TODO: relax assumption, that all data to be used together is stored in the same directory
+    for i_filenames in filenames[i_searchterm]:
+
+            directory = re.search('.*/(?=' + searchpattern +')',
+                                      i_filenames)
+            if directory:
+                directory_string = directory.group()
+                directories.add(directory_string)
+        #store all filepaths in each directory
+            for i_directories in directories:
+                filepath= re.search(searchpattern, i_filenames)
+                if filepath:
+                    filepath_string = filepath.string
+                    time_period = re.search('(\d{4})(\d{4})(-)(\d{4})(\d{4})(.nc)$',filepath_string)
+                    start_year = int(time_period.group(1))
+                    final_year = int(time_period.group(4))
+                    timespan = (start_year,final_year)
+                    timeperiods.add(timespan)
+                    # TODO: relax assumption, that all data ends with YYYYMMDD - YYYYMMDD.nc encoded date and only whole years are relevant for the data, with the same time period for all data
+                    searchresults[directory_string,str(timespan),i_searchterm] = {"filepath":filepath_string}
+timeperiods_list = list(timeperiods)
+
+# create setting files for all directories, timespans and searchterms, TODO: check for simplification, redundancy reduction
+directory_iterator = 0
+
+for i_directories in directories:
+    for i_timespan in timeperiods_list:
+        index = "dir"+str(directory_iterator) + "_" + str(i_timespan[0]) + str(i_timespan[1])
+        for i_searchterm in args.searchterms:
+            if (i_directories,str(i_timespan),i_searchterm) in searchresults.keys():
+                settings["input"][i_searchterm]= searchresults[i_directories,str(i_timespan),i_searchterm]["filepath"]
+            # modify years
+                settings ["years"]={"years":{"from":i_timespan[0],"to":i_timespan[1]}}
+
+            # specify individual code for file and output
 
 
-# save file paths
-for i_searchterm in range(len(args.searchterms)):
-    settings["input"][args.searchterms[i_searchterm]]= searchresults[args.searchterms[i_searchterm]]
-# modify years TODO: implement auto recognition of years
-    settings ["years"]["from"]=2095
-    settings["years"]["to"] = 2099
-# modify output file
-    settings["output"]["file"] = args.outputfile
-# save new settings file
-with open("settings.yml", "w") as output:
-        yaml.dump(settings, output)
-
+    # modify output file
+        settings["output"]["file"] = args.outputfile+index
+    # save new settings file
+        with open("settings"+index+".yml", "w") as output:
+            yaml.dump(settings, output)
+    directory_iterator += 1
